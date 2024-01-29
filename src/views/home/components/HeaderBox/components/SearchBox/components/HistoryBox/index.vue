0e08e07161a10bae05efc766a6cdc65ee114c317
Pzc_Neo <pzc_neo@foxmail.com>
2024-01-30 06:08:24 +0800

    添加设置搜索历史条数；修改删除搜索历史的方式。

--- src/views/home/components/HeaderBox/components/SearchBox/components/HistoryBox/index.vue
+++ src/views/home/components/HeaderBox/components/SearchBox/components/HistoryBox/index.vue
@@ -16,17 +16,13 @@
         <span class="word">
           {{ history }}
         </span>
-        <span
-          v-if="isShowDelete"
-          class="delete"
-          @click.stop="removeByIndex(index)"
-          >x</span
-        >
+        <!-- v-if="isShowDelete" -->
+        <div class="delete" @click.stop="removeByIndex(index)">x</div>
       </div>
     </div>
     <div class="option_list">
-      <span class="hint">搜索历史保存在浏览器本地</span>
-      <span class="option" @click="isShowDelete = !isShowDelete">管理</span>
+      <span class="hint">搜索历史只保存在浏览器本地</span>
+      <span class="option" @click="setQuantity">设置</span>
       <span class="option" @click="clearHistoryList">清空</span>
     </div>
     <el-button
@@ -51,6 +47,7 @@ export default {
     return {
       isShow: false,
       isShowDelete: false,
+      historyLength: 30,
       historyList: ['test', '123']
     }
   },
@@ -63,10 +60,26 @@ export default {
   },
   methods: {
     getHistoryList() {
-      const historyList = localStorage.getItem('historyList')
-      if (historyList !== '') {
-        this.historyList = JSON.parse(historyList) || []
+      const historyListStr = localStorage.getItem('historyList')
+      const historyLength = localStorage.getItem('historyLength')
+      if (historyLength) {
+        this.historyLength = parseInt(historyLength)
+      } else {
+        this.historyLength = 30
+        this.saveHistoryLength(this.historyLength)
       }
+      if (historyListStr !== '') {
+        let historyList = JSON.parse(historyListStr) || []
+        historyList = Array.from(new Set(historyList))
+        if (historyList.length > this.historyLength) {
+          this.historyList = historyList.slice(0, this.historyLength)
+        } else {
+          this.historyList = historyList
+        }
+      }
+    },
+    saveHistoryLength(length) {
+      localStorage.setItem('historyLength', length)
     },
     //
     moveToFirst(index) {
@@ -90,7 +103,7 @@ export default {
         return
       }
 
-      if (this.historyList.length > 30) {
+      if (this.historyList.length >= this.historyLength) {
         this.historyList.pop()
       }
       this.historyList.unshift(word)
@@ -107,9 +120,32 @@ export default {
       this.historyList.splice(index, 1)
       this.updateHistoryList()
     },
+    setQuantity() {
+      this.$prompt('请输入保存的搜索历史数量', '设置', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputValue: this.historyLength,
+        inputType: 'number',
+        inputPlaceholder: '请输入保存的搜索历史数量'
+      }).then(({ value }) => {
+        if (value) {
+          value = value > 0 ? value : 30
+          this.historyList = this.historyList.slice(0, value)
+          this.historyLength = value
+          this.saveHistoryLength(value)
+          this.updateHistoryList()
+        }
+      })
+    },
     clearHistoryList() {
-      this.historyList = []
-      this.updateHistoryList()
+      this.$confirm('此操作将清空搜索历史，是否继续？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.historyList = []
+        this.updateHistoryList()
+      })
     },
     handleSearch(word, index) {
       this.$emit('update:searchValueLocal', word)
@@ -137,10 +173,24 @@ export default {
     transition: top 0.3s ease-in-out;
     cursor: pointer;
     .delete {
-      padding: 3px;
+      display: none;
+      position: absolute;
+      right: -6px;
+      top: -5px;
+      border-radius: 50%;
+      width: 1.1em;
+      height: 1.1em;
+      line-height: 1em;
+      text-align: center;
+      margin: auto;
+      background: #f25a5a;
+      color: white;
     }
     &:hover {
       top: -2px;
+      .delete {
+        display: block;
+      }
     }
   }
 }
@@ -157,6 +207,7 @@ export default {
   }
 
   .option {
+    user-select: none;
     padding: 3px 5px;
     cursor: pointer;
   }
